59179a93300fd241ea53ea168b983f8cfcf4a9b5
Sabadeepak <sabachandirasekar@gmail.com>
2024-06-19 10:50:53 +0530

    added login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TransactionTrackerComponent } from './components/Transactions/transaction-tracker/transaction-tracker.component';
+import { LoginComponent } from './components/Login-Page/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    { path : "transactionTracker", component:TransactionTrackerComponent },
+    { path : "loginPage" , component:LoginComponent }
+
+];
--- src/app/components/Login-Page/login/login.component.ts
+++ src/app/components/Login-Page/login/login.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  isSignup = false;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      username: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+      const username = this.loginForm.value.username;
+
+      if (this.isSignup) {
+
+        
+      } else {
+
+
+      }
+    }
+  }
+
+  navigateToSignUp() {
+    this.isSignup = !this.isSignup;
+  }
 }
+
